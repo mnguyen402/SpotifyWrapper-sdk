6b83cfda86ea824cb7e3e4c0d81916523bec27fe
Ekta Raj <ektaraj@gmail.com>
2024-04-10 10:17:53 -0400

    Holiday pages added

--- app/src/main/java/com/example/spotify_sdk/HomePageActivity.java
+++ app/src/main/java/com/example/spotify_sdk/HomePageActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -13,13 +14,17 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        // "Let's Muse" button and set its click listener
         Button btnLetsMuse = findViewById(R.id.btnLetsMuse);
         btnLetsMuse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Intent to navigate to the Login activity
                 Intent intent = new Intent(HomePageActivity.this, Login.class);
                 startActivity(intent);
             }
         });
+
     }
 }
+
